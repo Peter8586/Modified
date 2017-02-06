750c61c4e75a3b3a2d2280853465a237433c7965
Peter8586 <peterpineda@Peters-MacBook-Pro-2.local>
2017-02-06 12:56:21 -0800

    First commit

---
+++ Modified/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Modified
+//
+//  Created by Peter Pineda on 2/6/17.
+//  Copyright © 2017 Peter Pineda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+            print("Hi")
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
